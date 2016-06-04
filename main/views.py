aeb9ea52cdd0814fb7e02f3c627c2c3b7b419d5a
GigaFlower <a_gigantic_flower@yeah.net>
2016-06-04 23:06:28 +0800

    Bug fix

--- main/views.py
+++ main/views.py
@@ -209,7 +209,7 @@ def unset_match(request, target_id):
 
     a.match_status = Photo.NONE
     a.match_key = ""
-    a.match_file_name = ""
+    a.matched_id = ""
     a.save()
 
     return HttpResponse("Unmatch success!")
